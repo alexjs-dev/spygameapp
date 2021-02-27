044847f1f0324bef6c54227a32650773252a20b0
Alex Dmitrijev <aleksei.dmitrijev@heathmont.net>
2021-02-27 20:53:02 +0200

    init

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,114 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useState } from "react";
+import _ from "lodash";
+import styles from "../styles/Home.module.scss";
+
+const words = [
+  "Торговый центр",
+  "База террористов",
+  "Банк",
+  "Больница",
+  "Воинская часть",
+  "Выставка  настольных игр",
+  "Войско крестоносцев",
+  "Зоопарк",
+  "Казино",
+  "Карнавал ",
+  "Карнавал",
+  "Киностудия",
+  "Корпоративная вечеринка",
+  "Лунапарк",
+  "Ночной клуб",
+  "Овощебаза",
+  "Океанский лайнер",
+  "Орбитальная станция",
+  "Отель",
+  "Партизанский отряд",
+  "Пассажирский поезд",
+  "Пиратский корабль",
+  "Пляж",
+  "Подводная лодка",
+  "Полицейский участок",
+  "Полярная станция",
+  "Посольство",
+  "Ресторан",
+  "Самолет",
+  "Спа-салон",
+  "Станция техобслуживания",
+  "Супермаркет",
+  "Театр",
+  "Хоккейная арена",
+  "Университет",
+  "Церковь",
+  "Цирк-шапито",
+  "Школа",
+];
+const word = _.sample(words);
 
 export default function Home() {
+  const [playerCount, setPlayerCount] = useState(0);
+  const [isGame, setGame] = useState(false);
+  const [spyIndex, setSpyIndex] = useState(0);
+  const [currentPlayer, setCurrentPlayer] = useState(0);
+  const [showWord, setShowWord] = useState(false);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Spy game</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      {!isGame && (
+        <>
+          <label for="count">Количество игроков</label>
+          <input
+            type="number"
+            id="count"
+            value={playerCount}
+            onChange={(e) => setPlayerCount(e.target.value)}
+          ></input>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+          <button
+            onClick={() => {
+              setSpyIndex(_.random(0, _.toNumber(playerCount)));
+              setGame(true);
+            }}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            Начать играть
+          </button>
+        </>
+      )}
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+      {isGame && (
+        <>
+          <button
+            onClick={() => {
+              setShowWord(true);
+            }}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+            Показать слово. Игрок {currentPlayer + 1}
+          </button>
+          <button
+            onClick={() => {
+              setShowWord(false);
+            }}
+          >
+            Спрятать
+          </button>
+          <button
+            onClick={() => {
+              setShowWord(false);
+              setCurrentPlayer(currentPlayer + 1);
+            }}
+          >
+            След игрок
+          </button>
+          {showWord && (
+            <span> {spyIndex === currentPlayer ? "Вы шпион" : word}</span>
+          )}
+        </>
+      )}
     </div>
-  )
+  );
 }
